6783c04431eff9e8219b335550fcd64d77b41664
Saibal kole <saibalkole@gmail.com>
2024-09-29 02:10:26 +0530

    creates getAllPost, getPostById controller and its router

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -27,6 +27,9 @@ const uploadPost = asyncHandler(async(req,res)=>{
 const updatePost = asyncHandler(async(req,res)=>{
     const id = req.params.id;
     const {title,content} = req.body;
+    if(!id) {
+        throw new ApiError(402,"Please provide the post unique id");
+    }
     if(!title && !content){
         throw new ApiError(402,"Please update atleast title or Content");
     }
@@ -37,6 +40,37 @@ const updatePost = asyncHandler(async(req,res)=>{
     if(title) post.title = title;
     if(content) post.content = content;
     const updatedPost = await post.save();
+    if(!updatedPost){
+        throw new ApiError(505,"Post update failed!")
+    }
+        
     res.status(200).json(new ApiResponse(200,{response:updatedPost},"Your Post is Successfully Updated!"));
 })
-export {uploadPost,updatePost};
+const deletePost = asyncHandler(async(req,res)=>{
+    const id = req.params.id;
+    if(!id) throw new ApiError(404,"Please Provide post Id");
+
+    const post = await Post.findById(id);
+    if(!req.user._id.equals(post.owner)){
+        throw new ApiError(405,"You are not authorized to delete this Post!")
+    }
+    const deletedPost = await Post.findByIdAndDelete(id);
+    res.status(200).json(new ApiResponse(200,{},"The Post is successfully deleted"));
+})
+const getAllPost = asyncHandler(async(req,res)=>{
+    const id = req.user._id;
+    const allPosts = await Post.find({owner:id});
+    res.status(202).json(new ApiResponse(202,{response:allPosts},"Successfully Fetched all Posts"))
+})
+const getPostById = asyncHandler(async(req,res)=>{
+    const postId = req.params.id;
+    if(!postId){
+        throw new ApiError(400,"Please Provide valid post Id");
+    }
+    console.log(req.params);
+    const post = await Post.findById(postId);
+    console.log(post);
+    if(!post) throw new ApiError(500,"The post id is not valid");
+    res.status(200).json(new ApiResponse(200,{response:post},"Post fetched successefully!"))
+})
+export {uploadPost,updatePost,deletePost,getAllPost,getPostById};
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import { updatePost, uploadPost } from "../controllers/post.controller.js";
+import { deletePost, getAllPost, getPostById, updatePost, uploadPost } from "../controllers/post.controller.js";
 
 const router = Router();
 router.use(verifyJWT); // this actually checks user jwt for all this routes
 
+router.route('/').get(getAllPost);
+router.route('/:id').get(getPostById);
 router.route('/upload').post(
     upload.fields([
         {
@@ -20,4 +22,5 @@ router.route('/upload').post(
     uploadPost
 );
 router.route('/update/:id').patch(updatePost);
+router.route('/delete/:id').delete(deletePost);
 export default router;
